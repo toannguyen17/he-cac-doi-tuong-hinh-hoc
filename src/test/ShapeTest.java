f21cf67df894e39de9b0fcb414ce647398840c01
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-05-04 08:58:17 +0700

    add project

---
+++ src/test/ShapeTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import app.Shape;
+
+public class ShapeTest {
+	public static void main(String[] args) {
+		Shape shape = new Shape();
+		System.out.println(shape);
+
+		shape = new Shape("red", false);
+		System.out.println(shape);
+	}
+}
